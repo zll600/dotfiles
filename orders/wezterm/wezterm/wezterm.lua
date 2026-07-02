2a339b89a4bd0026d1f65c37a58fd76290273802
zll600 <3400692417@qq.com>
2026-07-02 22:51:38 +0800

    feat: support wezterm

---
+++ orders/wezterm/wezterm/wezterm.lua
@@ -0,0 +1,19 @@
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration
+local config = wezterm.config_builder()
+
+-- Set font size
+config.font_size = 14
+
+-- Set color scheme (e.g., 'Catppuccin Mocha', 'Tokyo Night', or 'Dracula')
+config.color_scheme = 'Catppuccin Mocha'
+
+-- Make the tab bar look cleaner (optional)
+config.use_fancy_tab_bar = false
+
+-- Hide the native window title bar for a sleeker look
+config.window_decorations = 'RESIZE'
+
+-- Finally, return the configuration to WezTerm
+return config
